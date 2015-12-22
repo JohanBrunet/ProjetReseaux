1b088acf209cdbb1f06f1305c68f34c6f68ea4d3
JohanBrunet <brunet.johan.23@gmail.com>
2015-12-22 16:12:06 +0100

    Création des premières classes

---
+++ ProjetReseaux/src/fact/FactClient.java
@@ -0,0 +1,12 @@
+package fact;
+
+public class FactClient {
+	
+	public static void main(String[] args) {
+		String serverAdress;
+		int serverPort;
+		
+		serverAdress = args[0];
+	}
+
+}
---
+++ ProjetReseaux/src/fact/FactServer.java
@@ -0,0 +1,9 @@
+package fact;
+
+public class FactServer {
+
+	public static void main(String[] args) {
+		// TODO Ecrire le serveur qui permettra de calculer une factorielle
+
+	}
+}
