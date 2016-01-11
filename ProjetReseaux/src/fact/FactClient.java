d9de5fa0f1c114be3cd0482f3053aaf3302d0fa5
JohanBrunet <brunet.johan.23@gmail.com>
2016-01-11 16:17:16 +0100

    MaJ FactClient

--- ProjetReseaux/src/fact/FactClient.java
+++ ProjetReseaux/src/fact/FactClient.java
@@ -1,21 +1,68 @@
 package fact;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class FactClient {
-	
-	private String serverAddress;
-	private int serverPort;
-	
-	public FactClient(String address, int port) {
-		this.serverAddress = address;
-		this.serverPort = port;
+
+	public class Listen extends Thread {
+
+		Socket socket;
+		InputStream sInput;
+
+		public Listen(Socket socket) {
+			try {
+				this.socket = socket;
+				sInput  = socket.getInputStream();
+			} catch (Exception e) {}
+		}
+
+		@SuppressWarnings("resource")
+		public void run() {
+			Scanner sc = new Scanner(sInput);
+			while (true) {
+				if (sc.hasNext()) {
+					String msg = sc.nextLine();
+					System.out.println(msg);
+				}
+			}
+		}
 	}
-	
+
 	public void run() {
-		
+		InetAddress address;
+		Socket socket;
+		Listen listen;
+		Scanner scanner;
+		String msg;
+
+		try {
+			address = InetAddress.getLocalHost();
+			socket = new Socket(address, 50000);
+			listen = new Listen(socket);
+			listen.start();
+			PrintStream output = new PrintStream(socket.getOutputStream());
+			scanner = new Scanner(System.in);
+			while (true) {
+				msg = scanner.nextLine();
+				output.println(msg);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public static void main(String[] args) {
-		FactClient client = new FactClient(args[0], Integer.parseInt(args[1]));
-		client.run();
+
+	public static void main(String argv[]) {
+		try {
+			FactClient c = new FactClient();
+			c.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
+
