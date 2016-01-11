32a919f1e46bb58e44ba00ffd9a22bd6455c081c
JohanBrunet <brunet.johan.23@gmail.com>
2016-01-11 16:26:04 +0100

    MaJ FactServer -- Modifications nécessaires pour répondre au cahier des charges

--- ProjetReseaux/src/fact/FactServer.java
+++ ProjetReseaux/src/fact/FactServer.java
@@ -1,9 +1,85 @@
 package fact;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FactServer {
 
-	public static void main(String[] args) {
-		// TODO Ecrire le serveur qui permettra de calculer une factorielle
+	// Client part
 
+	public class ClientThread extends Thread {
+
+		Socket socket;
+		InputStream sInput;
+		OutputStream sOutput;
+		String name;
+
+		public ClientThread(Socket socket, String name) {
+			try {
+				this.socket = socket;
+				sOutput = socket.getOutputStream();
+				sInput  = socket.getInputStream();
+				this.name = name;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		@SuppressWarnings("resource")
+		public void run() {
+			Scanner sc = new Scanner(sInput);
+			while (true)
+				if (sc.hasNext()) {
+					String msg = sc.nextLine();
+					System.out.println(name + ": " + msg);
+					broadcast(name + ": " + msg);
+				}
+		}
+	}
+
+	// Server part
+
+	ArrayList<ClientThread> socks = new ArrayList<ClientThread>();
+	int port;
+	int num = 0;
+
+	public FactServer(int port) {
+		this.port = port;
+	}
+
+	public synchronized void broadcast(String msg) {
+		for (ClientThread e : socks) {
+			PrintStream output = new PrintStream(e.sOutput);
+			output.println(msg);
+		}
+	}
+
+	@SuppressWarnings("resource")
+	public void run() {
+		try {
+			ServerSocket sServer = new ServerSocket(port);
+			while (true) {
+				Socket s = sServer.accept();
+				ClientThread c = new ClientThread(s, "Thread " + num);;
+				System.out.println("Connection of client " + num);
+				num++;
+				socks.add(c);
+				c.start();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String [] argv) {
+		FactServer c = new FactServer (50000);
+		c.run();
 	}
 }
+
+
