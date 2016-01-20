b3dd0e71a188fd6da02fe9aa24d5fab2aa62e500
quentin.bouygues <quentin.bouygues@igtp6-pc04.isim.intra>
2016-01-20 17:55:55 +0100

    Modif factclient demande fact

--- ProjetReseaux/src/fact/FactClient.java
+++ ProjetReseaux/src/fact/FactClient.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Classe permettant de créer un client pour la demande de calcul de factorielle au serveur
@@ -17,19 +18,23 @@ public class FactClient {
 	private int port;
 	private int fact;
 	
-	public FactClient(String address, int port, int fact) {
+	public FactClient(String address, int port) {
 		this.address = address;
 		this.port = port;
-		this.fact = fact;
+		this.fact = 0;
 	}
 	
 	public void askFact() {
 		InetAddress address;
 		Socket socket;
-
 		try {
 			address = InetAddress.getByName(this.address);
 			socket = new Socket(address, this.port);
+			System.out.println("Entrez le nombre dont vous voulez la factorielle :\n");
+			Scanner sc = new Scanner(System.in);
+			if(sc.hasNext()) {
+				this.fact = Integer.parseInt(sc.nextLine());
+			}
 			PrintStream output = new PrintStream(socket.getOutputStream());
 			while (true) {
 				output.println(this.fact);
@@ -41,7 +46,7 @@ public class FactClient {
 
 	public static void main(String argv[]) {
 		try {
-			FactClient client = new FactClient(argv[0], Integer.parseInt(argv[1]), Integer.parseInt(argv[2]));
+			FactClient client = new FactClient(argv[0], Integer.parseInt(argv[1]));
 			client.askFact();
 		} catch (Exception e) {
 			e.printStackTrace();
--- ProjetReseaux/src/fib/FibClient.java
+++ ProjetReseaux/src/fib/FibClient.java
@@ -1,5 +1,45 @@
 package fib;
 
-public class FibClient {
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
+import fib.FibClient;
+
+public class FibClient {
+	private String address;
+	private int port;
+	private int fib;
+	
+	public FibClient(String address, int port, int fib) {
+		this.address = address;
+		this.port = port;
+		this.fib = fib;
+	}
+	
+	public void askFact() {
+		InetAddress address;
+		Socket socket;
+
+		try {
+			address = InetAddress.getByName(this.address);
+			socket = new Socket(address, this.port);
+			PrintStream output = new PrintStream(socket.getOutputStream());
+			while (true) {
+				output.println(this.fib);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String argv[]) {
+		try {
+			FibClient client = new FibClient(argv[0], Integer.parseInt(argv[1]), Integer.parseInt(argv[2]));
+			client.askFact();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
